4918a8f878fa35f72627069251d713d8d10bf7c0
NathanScarrow <nathan.scarrow@uleth.ca>
2023-11-25 16:43:26 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log("connected!")
+var x = document.title;
+console.log(x)
+
+var myImage = document.querySelector('#imageid');
+
+function FirstImage() {
+    myImage.src="images/pose_oneposeone.png";
+}
+function SecondImage() {
+  myImage.src="images/pose_twoposeone.png";
+}
+function ThirdImage() {
+  myImage.src="images/pose_threeposeone.png";
+}
+function FourthImage() {
+  myImage.src="images/pose_fourposeone.png";
+}
+function FifthImage() {
+  myImage.src="images/pose_fiveposeone.png";
+}
